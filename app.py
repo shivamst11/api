21101f26af1e9715f459d6835e583bcc805f0e40
shivam triapthi <71796529+shivamst11@users.noreply.github.com>
2021-05-21 08:02:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
-import os
 
+from security import authenticate, identity
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-
-from security import authenticate, identity
+import datetime
 from resources.user import UserRegister
-from resources.item import Item, ItemList
-from db import db
-from resources.store import Store, StoreList
+from resources.items import Item,Itemlist
 
-app = Flask(__name__)
 
-app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
-api = Api(app)
+app=Flask(__name__)
+app.secret_key="shivam"
+api=Api(app)
+jwt=JWT(app,authenticate,identity) #/auth
 
-jwt = JWT(app, authenticate, identity)  # /auth
 
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
+app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(seconds=1000)
+#for jwt token expire time can use day ex.  (days=10)
 
-api.add_resource(UserRegister, '/register')
 
-if __name__ == '__main__':
-    
-    db.init_app(app)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+items =[{'name':'urdu'},{'name':'urdu'},{'name':'hindi'}]
 
-    app.run(port=5000)
+
+        
+        
+        
+api.add_resource(Item,'/<string:name>')
+api.add_resource(Itemlist,'/')
+api.add_resource(UserRegister,'/Register')
+app.run(debug=True,port=7071)
